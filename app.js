8afe0516ea40242ce515472eda8e362ba2d4ed69
IanFonzie <fberg_i@hotmail.com>
2021-11-10 12:53:50 -0800

    Enable CORS.

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Add server name to request.
 app.use((req, res, next) => {
   req.serverName = () => `${req.protocol}://${req.get('host')}`;
