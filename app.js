505740ac895d690e889943904de4ea04b894bc22
IanFonzie <fberg_i@hotmail.com>
2021-12-02 23:31:33 -0800

    Specific build directory for React app.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 // Enable CORS
 app.use(function(req, res, next) {
